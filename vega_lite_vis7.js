14109fc0c2a0d93c419c708e3bef5b2c1dd60fde
Chong How <125256171+Chonghow03@users.noreply.github.com>
2024-10-21 18:24:01 +0800

    Add Data Visualization About Water Use And Stress

---
+++ vega_lite_vis7.js
@@ -0,0 +1,5 @@
+var vg_6 = "json/agricultural_land_irrigation.vg.json";
+    
+vegaEmbed("#agricultural_land_irrigation", vg_6).then(function(result) {
+    // Access the Vega view instance
+}).catch(console.error);
